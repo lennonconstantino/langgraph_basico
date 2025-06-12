df3f8a6ea7ded8a3e0bb6004affe69afbd559a08
lennonconstantino <lennonconstantino@gmail.com>
2025-06-12 15:33:12 -0300

    first examples

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ artificial intelligence
 
 Criando o ambiente
 python -m venv .venv
+python3.11 -m venv .venv
+source .venv/bin/activate
 pip list
-pip install -r requirements.txt
+pip install -r ./requirements.txt
 
---
+++ exemplo.py
@@ -0,0 +1,39 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from langgraph.graph import StateGraph
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+# definir o modelo
+llm_model = ChatOpenAI(model="gpt-3.5-turbo", api_key=API_KEY)
+
+# definicao do StateGraph
+class GraphState(BaseModel):
+    input: str
+    output: str
+
+# funcao de resposta
+def responder(state):
+    input_message = state.input
+    response = llm_model.invoke([HumanMessage(content=input_message)])
+    return GraphState(input=state.input, output=response.content)
+
+# criando o graph
+graph = StateGraph(GraphState)
+graph.add_node("responder", responder)
+graph.set_entry_point ("responder")
+graph.set_finish_point ("responder")
+
+# Compilando o Grafo
+export_graph = graph.compile()
+
+print("Test")
+
+# testando o agente
+if __name__ == "__main__":
+    result = export_graph.invoke(GraphState(input="Quem descobriu a América", output=""))
+    print(result)
